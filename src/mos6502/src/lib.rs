f9cccff3dd456c272be74a4276fb891f618b40d1
Paulo Almeida <paulo.almeidarodenas@niwa.co.nz>
2022-04-06 16:26:06 +1200

    [mos6502] :: refactor struct members

--- src/mos6502/src/lib.rs
+++ src/mos6502/src/lib.rs
@@ -5,24 +5,24 @@ use opcodes::{Instruction, AddressingMode, parse_instruction};
 
 #[derive(Debug)]
 pub struct Mos6502 {
-    reg_a: u8,
-    reg_x: u8,
-    reg_y: u8,
+    a: u8,
+    x: u8,
+    y: u8,
     pc: u16,
     sp: u8,
-    st_flags: u8,
+    flags: u8,
 }
 
 impl Mos6502 {
     pub fn new() -> Self {
         //TODO find out default values for the CPU
         Mos6502 {
-            reg_a: 0x0,
-            reg_x: 0x0,
-            reg_y: 0x0,
+            a: 0x0,
+            x: 0x0,
+            y: 0x0,
             pc: 0x0,
-            sp: 0x0,
-            st_flags: 0x0,
+            sp: 0xFD,
+            flags: 0x34,
         }
     }
 
