bd6f7a6f84aa77297665eece2f83d90b95a0147a
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2022-04-15 16:18:56 +1200

    [mos6502] :: implement Absolute X addressing mode for AND instruction

--- src/mos6502/src/opcodes/alu.rs
+++ src/mos6502/src/opcodes/alu.rs
@@ -16,20 +16,36 @@ pub fn adc(cpu: &mut Mos6502, inst: Instruction, _bus: &mut Bus) -> u8 {
 
 pub fn and(cpu: &mut Mos6502, inst: Instruction, bus: &mut Bus) -> u8 {
     println!("{} -> {:?} was called with cpu: {:?}", inst.name, inst.mode, cpu);
+    let mut additional_cycle = 0_u8;
 
     let fetched= match inst.mode {
-        Immediate => bus.read_address(cpu.pc + 1),
+        Immediate => bus.read_u8(cpu.pc + 1),
         ZeroPage => {
-            let addr = bus.read_address(cpu.pc + 1);
-            bus.read_address(addr as u16)
+            let addr = bus.read_u8(cpu.pc + 1);
+            bus.read_u8(addr as u16)
         },
         ZeroPageX => {
-            // val = PEEK((arg + X) % 256)
-            let mut addr = bus.read_address(cpu.pc + 1) as u16;
+            // val = PEEK((arg + X) % 256) to simulate hardware bug in 6502
+            let mut addr = bus.read_u8(cpu.pc + 1) as u16;
             addr = (addr + cpu.x as u16) % 256;
-            bus.read_address(addr)
+            bus.read_u8(addr)
+        },
+        Absolute => {
+            let addr = bus.read_u16(cpu.pc + 1);
+            bus.read_u8(addr)
+        },
+        AbsoluteX => {
+            let orig_addr = bus.read_u16(cpu.pc + 1);
+            let addr = orig_addr + cpu.x as u16;
+
+            // page crossing costs 1 additional cycle.. Joao would be proud of me now <3
+            if (orig_addr >> 8) != (addr >> 8) {
+                additional_cycle = 1;
+            }
+
+            bus.read_u8(addr)
         }
-        _ => panic!("not supported"),
+        _ => panic!("invalid addressing mode... aborting"),
     };
 
     cpu.a = cpu.a & fetched;
@@ -42,7 +58,7 @@ pub fn and(cpu: &mut Mos6502, inst: Instruction, bus: &mut Bus) -> u8 {
         cpu.set_flag(Negative);
     }
     cpu.pc += inst.bytes as u16;
-    0
+    additional_cycle
 }
 
 pub fn eor(cpu: &mut Mos6502, inst: Instruction, _bus: &mut Bus) -> u8 {
@@ -89,7 +105,7 @@ mod test{
         cpu.flags = 0b0000_0000;
         cpu.a = 0b0000_1100;
         cpu.pc = 0x10;
-        bus.write_address(cpu.pc + 1, 0b0000_1000);
+        bus.write_u8(cpu.pc + 1, 0b0000_1000);
         let cycles = cpu.execute_instruction(opcode.opcode, &mut bus);
         assert_eq!(cycles, opcode.cycles);
         assert_eq!(cpu.a, 0b0000_1000);
@@ -103,7 +119,7 @@ mod test{
         cpu.flags = 0b0000_0000;
         cpu.a = 0b1111_1111;
         cpu.pc = 0x10;
-        bus.write_address(cpu.pc + 1, 0b0000_0000);
+        bus.write_u8(cpu.pc + 1, 0b0000_0000);
         let cycles = cpu.execute_instruction(opcode.opcode, &mut bus);
         assert_eq!(cycles, opcode.cycles);
         assert_eq!(cpu.a, 0b0000_0000);
@@ -117,7 +133,7 @@ mod test{
         cpu.flags = 0b0000_0000;
         cpu.a = 0b1111_1111;
         cpu.pc = 0x10;
-        bus.write_address(cpu.pc + 1, 0b1000_0000);
+        bus.write_u8(cpu.pc + 1, 0b1000_0000);
         let cycles = cpu.execute_instruction(opcode.opcode, &mut bus);
         assert_eq!(cycles, opcode.cycles);
         assert_eq!(cpu.a, 0b1000_0000);
@@ -132,8 +148,8 @@ mod test{
         cpu.flags = 0b0000_0000;
         cpu.a = 0b1111_1111;
         cpu.pc = 0x0800;
-        bus.write_address(cpu.pc + 1, 0x10);
-        bus.write_address(0x0010, 0b0000_1111); // write to zero page
+        bus.write_u8(cpu.pc + 1, 0x10);
+        bus.write_u8(0x0010, 0b0000_1111); // write to zero page
         let cycles = cpu.execute_instruction(opcode.opcode, &mut bus);
         assert_eq!(cycles, opcode.cycles);
         assert_eq!(cpu.a, 0b0000_1111);
@@ -149,8 +165,8 @@ mod test{
         cpu.a = 0b1111_1111;
         cpu.x = 0x01;
         cpu.pc = 0x0800;
-        bus.write_address(cpu.pc + 1, 0x10);
-        bus.write_address(0x11, 0b0000_1111); // write to zero page
+        bus.write_u8(cpu.pc + 1, 0x10);
+        bus.write_u8(0x11, 0b0000_1111); // write to zero page
         let cycles = cpu.execute_instruction(opcode.opcode, &mut bus);
         assert_eq!(cycles, opcode.cycles);
         assert_eq!(cpu.a, 0b0000_1111);
@@ -165,13 +181,63 @@ mod test{
         cpu.a = 0b1111_1111;
         cpu.x = 0xFF;
         cpu.pc = 0x0800;
-        bus.write_address(cpu.pc + 1, 0x10);
-        bus.write_address(0xF, 0b0001_1111); // write to zero page
+        bus.write_u8(cpu.pc + 1, 0x10);
+        bus.write_u8(0xF, 0b0001_1111); // write to zero page
         let cycles = cpu.execute_instruction(opcode.opcode, &mut bus);
         assert_eq!(cycles, opcode.cycles);
         assert_eq!(cpu.a, 0b0001_1111);
         assert_eq!(cpu.flags, 0b0000_0000);
         assert_eq!(cpu.pc, 0x0802);
+        assert_eq!(cpu.sp, 0xff);
+
+        // Absolute mode, no flags
+        let opcode = OPTABLE[0x2D];
+        let (mut cpu, mut bus) = init();
+        cpu.sp = 0xff;
+        cpu.flags = 0b0000_0000;
+        cpu.a = 0b1111_1111;
+        cpu.pc = 0x0800;
+        bus.write_u16(cpu.pc + 1, 0x1234);
+        bus.write_u8(0x1234, 0b0001_1111);
+        let cycles = cpu.execute_instruction(opcode.opcode, &mut bus);
+        assert_eq!(cycles, opcode.cycles);
+        assert_eq!(cpu.a, 0b0001_1111);
+        assert_eq!(cpu.flags, 0b0000_0000);
+        assert_eq!(cpu.pc, 0x0803);
+        assert_eq!(cpu.sp, 0xff);
+
+        // Absolute X mode, no flags
+        let opcode = OPTABLE[0x3D];
+        let (mut cpu, mut bus) = init();
+        cpu.sp = 0xff;
+        cpu.flags = 0b0000_0000;
+        cpu.a = 0b1111_1111;
+        cpu.x = 0x10;
+        cpu.pc = 0x0800;
+        bus.write_u16(cpu.pc + 1, 0x1234);
+        bus.write_u8(0x1244, 0b0001_1111);
+        let cycles = cpu.execute_instruction(opcode.opcode, &mut bus);
+        assert_eq!(cycles, opcode.cycles);
+        assert_eq!(cpu.a, 0b0001_1111);
+        assert_eq!(cpu.flags, 0b0000_0000);
+        assert_eq!(cpu.pc, 0x0803);
+        assert_eq!(cpu.sp, 0xff);
+
+        // Absolute X mode, no flags, page crossed
+        let opcode = OPTABLE[0x3D];
+        let (mut cpu, mut bus) = init();
+        cpu.sp = 0xff;
+        cpu.flags = 0b0000_0000;
+        cpu.a = 0b1111_1111;
+        cpu.x = 0xFF;
+        cpu.pc = 0x0800;
+        bus.write_u16(cpu.pc + 1, 0x1234);
+        bus.write_u8(0x1333, 0b0001_1111);
+        let cycles = cpu.execute_instruction(opcode.opcode, &mut bus);
+        assert_eq!(cycles, opcode.cycles + 1);
+        assert_eq!(cpu.a, 0b0001_1111);
+        assert_eq!(cpu.flags, 0b0000_0000);
+        assert_eq!(cpu.pc, 0x0803);
         assert_eq!(cpu.sp, 0xff);
     }
 }
