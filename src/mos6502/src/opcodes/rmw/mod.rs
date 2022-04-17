97482eaec79ababec0d7b12157622687f6e8ea15
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2022-04-17 16:45:40 +1200

    [mos6502] :: move NOP to new structure

--- src/mos6502/src/opcodes/rmw/mod.rs
+++ src/mos6502/src/opcodes/rmw/mod.rs
@@ -1,7 +1,9 @@
 mod stx;
+mod nop;
 use bus::Bus;
 use super::{Mos6502, Instruction};
 pub use stx::*;
+pub use nop::*;
 
 //TODO implement actual functions here... right now I'm just interested in the scaffold
 
@@ -63,45 +65,4 @@ pub fn dex(cpu: &mut Mos6502, inst: Instruction, _bus: &mut Bus) -> u8 {
 pub fn inc(cpu: &mut Mos6502, inst: Instruction, _bus: &mut Bus) -> u8 {
     println!("{} -> {:?} was called with cpu: {:?}", inst.name, inst.mode, cpu);
     0
-}
-
-/// NOP - No Operation
-/// The NOP instruction causes no changes to the processor other than the normal incrementing
-/// of the program counter to the next instruction.
-pub fn nop(cpu: &mut Mos6502, inst: Instruction, _bus: &mut Bus) -> u8 {
-    println!("{} -> {:?} was called with cpu: {:?}", inst.name, inst.mode, cpu);
-    cpu.pc += 1;
-    0
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::opcodes::{OPTABLE};
-
-    fn init() -> (Mos6502, Bus) {
-        (Mos6502::new(), Bus::new())
-    }
-
-    fn common_execute(cpu: &mut Mos6502, bus: &mut Bus, opcode: usize) {
-        let opcode = OPTABLE[opcode];
-        let old_pc = cpu.pc;
-        let cycles = cpu.execute_instruction(opcode.opcode, bus);
-        assert_eq!(cycles, opcode.cycles);
-        assert_eq!(old_pc + opcode.bytes as u16, cpu.pc);
-    }
-
-    #[test]
-    fn test_nop() {
-        let (mut cpu, mut bus) = init();
-        cpu.flags = 0b0000_0000;
-        cpu.a = 0;
-        cpu.x = 0;
-        cpu.y = 0;
-        common_execute(&mut cpu, &mut bus, 0xea);
-        assert_eq!(cpu.flags, 0b0000_0000);
-        assert_eq!(cpu.a, 0);
-        assert_eq!(cpu.x, 0);
-        assert_eq!(cpu.y, 0);
-    }
 }
