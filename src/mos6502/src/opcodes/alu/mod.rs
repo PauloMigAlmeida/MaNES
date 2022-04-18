89e47cc1abb03bd41cab29404051d2bc31e5c8d2
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2022-04-18 16:02:44 +1200

    [mos6502] :: implement instruction SBC

--- src/mos6502/src/opcodes/alu/mod.rs
+++ src/mos6502/src/opcodes/alu/mod.rs
@@ -5,7 +5,7 @@ mod cmp;
 mod lda;
 mod sta;
 mod adc;
-use bus::Bus;
+mod sbc;
 use super::{Mos6502, Instruction};
 pub use and::*;
 pub use ora::*;
@@ -14,11 +14,5 @@ pub use cmp::*;
 pub use lda::*;
 pub use sta::*;
 pub use adc::*;
+pub use sbc::*;
 
-//TODO implement actual functions here... right now I'm just interested in the scaffold
-
-
-pub fn sbc(cpu: &mut Mos6502, inst: Instruction, _bus: &mut Bus) -> u8 {
-    println!("{} -> {:?} was called with cpu: {:?}", inst.name, inst.mode, cpu);
-    0
-}
