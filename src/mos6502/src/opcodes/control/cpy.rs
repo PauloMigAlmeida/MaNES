be77cd8291e750ec780f9a583f10d0b2f2a71613
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2022-04-26 19:11:20 +1200

    [mos6502] :: implement instruction CPY

--- src/mos6502/src/opcodes/control/cpy.rs
+++ src/mos6502/src/opcodes/control/cpy.rs
@@ -19,7 +19,7 @@ pub fn cpy(cpu: &mut Mos6502, inst: Instruction, bus: &mut Bus) -> u8 {
 
 #[cfg(test)]
 mod tests {
-    use crate::{Immediate, Absolute, AbsoluteX, ZeroPage, ZeroPageX};
+    use crate::{Immediate, Absolute, ZeroPage};
     use super::*;
     use crate::opcodes::{OPTABLE};
 
@@ -35,35 +35,35 @@ mod tests {
         assert_eq!(opcode.mode, Immediate);
         assert_eq!(opcode.name, OPCODE_NAME);
 
-        // no flags 
+        // Carry flag
         let (mut cpu, mut bus) = init();
         cpu.sp = 0xff;
         cpu.flags = 0b0000_0000;
-        cpu.y = 0;
+        cpu.y = 0x11;
         cpu.pc = 0x0800;
         bus.write_u8(cpu.pc + 1, 0x10);
         let cycles = cpu.execute_instruction(opcode.opcode, &mut bus);
         assert_eq!(cycles, opcode.cycles);
-        assert_eq!(cpu.y, 0);
-        assert_eq!(cpu.flags, 0b0000_0000);
+        assert_eq!(cpu.y, 0x11);
+        assert_eq!(cpu.flags, 0b0000_0001);
         assert_eq!(cpu.pc, 0x0802);
         assert_eq!(cpu.sp, 0xff);
 
-        // clear flags 
+        // clear flags -> Carry flag set
         let (mut cpu, mut bus) = init();
         cpu.sp = 0xff;
         cpu.flags = 0b1000_0010;
-        cpu.y = 0;
+        cpu.y = 0x11;
         cpu.pc = 0x0800;
         bus.write_u8(cpu.pc + 1, 0x10);
         let cycles = cpu.execute_instruction(opcode.opcode, &mut bus);
         assert_eq!(cycles, opcode.cycles);
-        assert_eq!(cpu.y, 0x10);
-        assert_eq!(cpu.flags, 0b0000_0000);
+        assert_eq!(cpu.y, 0x11);
+        assert_eq!(cpu.flags, 0b0000_0001);
         assert_eq!(cpu.pc, 0x0802);
         assert_eq!(cpu.sp, 0xff);
 
-        // zero flag set 
+        // Carry + Zero flags set
         let (mut cpu, mut bus) = init();
         cpu.sp = 0xff;
         cpu.flags = 0b0000_0000;
@@ -73,129 +73,64 @@ mod tests {
         let cycles = cpu.execute_instruction(opcode.opcode, &mut bus);
         assert_eq!(cycles, opcode.cycles);
         assert_eq!(cpu.y, 0x10);
-        assert_eq!(cpu.flags, 0b0000_0010);
+        assert_eq!(cpu.flags, 0b0000_0011);
         assert_eq!(cpu.pc, 0x0802);
         assert_eq!(cpu.sp, 0xff);
 
-        // // negative flag set
-        // let (mut cpu, mut bus) = init();
-        // cpu.sp = 0xff;
-        // cpu.flags = 0b0000_0000;
-        // cpu.y = 0;
-        // cpu.pc = 0x0800;
-        // bus.write_u8(cpu.pc + 1, 0b1000_0000);
-        // let cycles = cpu.execute_instruction(opcode.opcode, &mut bus);
-        // assert_eq!(cycles, opcode.cycles);
-        // assert_eq!(cpu.y, 0b1000_0000);
-        // assert_eq!(cpu.flags, 0b1000_0000);
-        // assert_eq!(cpu.pc, 0x0802);
-        // assert_eq!(cpu.sp, 0xff);
+        // negative flag set
+        let (mut cpu, mut bus) = init();
+        cpu.sp = 0xff;
+        cpu.flags = 0b0000_0000;
+        cpu.y = 0;
+        cpu.pc = 0x0800;
+        bus.write_u8(cpu.pc + 1, 0x10);
+        let cycles = cpu.execute_instruction(opcode.opcode, &mut bus);
+        assert_eq!(cycles, opcode.cycles);
+        assert_eq!(cpu.y, 0);
+        assert_eq!(cpu.flags, 0b1000_0000);
+        assert_eq!(cpu.pc, 0x0802);
+        assert_eq!(cpu.sp, 0xff);
     }
 
-    // implemenmt for other addressing modes...
-    // also, check if the overflowing_sub is what we should be doing here.. I still didn't get
-    // why 0 - 0x10 is resulting in 240 :=)
+    #[test]
+    fn zero_page() {
+        let opcode = OPTABLE[0xC4];
+        assert_eq!(opcode.mode, ZeroPage);
+        assert_eq!(opcode.name, OPCODE_NAME);
 
-    // #[test]
-    // fn zero_page() {
-    //     let opcode = OPTABLE[0xA4];
-    //     assert_eq!(opcode.mode, ZeroPage);
-    //     assert_eq!(opcode.name, OPCODE_NAME);
-    //
-    //     let (mut cpu, mut bus) = init();
-    //     cpu.sp = 0xff;
-    //     cpu.flags = 0b0000_0000;
-    //     cpu.y = 0b0000_1100;
-    //     cpu.pc = 0x0800;
-    //     bus.write_u8(cpu.pc + 1, 0x10);
-    //     bus.write_u8(0x10, 0b0000_1010);
-    //     let cycles = cpu.execute_instruction(opcode.opcode, &mut bus);
-    //     assert_eq!(cycles, opcode.cycles);
-    //     assert_eq!(cpu.y, 0b0000_1010);
-    //     assert_eq!(cpu.flags, 0b0000_0000);
-    //     assert_eq!(cpu.pc, 0x0802);
-    //     assert_eq!(cpu.sp, 0xff);
-    // }
+        let (mut cpu, mut bus) = init();
+        cpu.sp = 0xff;
+        cpu.flags = 0b0000_0000;
+        cpu.y = 0x11;
+        cpu.pc = 0x0800;
+        bus.write_u8(cpu.pc + 1, 0x10);
+        bus.write_u8(0x10, 0x10);
+        let cycles = cpu.execute_instruction(opcode.opcode, &mut bus);
+        assert_eq!(cycles, opcode.cycles);
+        assert_eq!(cpu.y, 0x11);
+        assert_eq!(cpu.flags, 0b0000_0001);
+        assert_eq!(cpu.pc, 0x0802);
+        assert_eq!(cpu.sp, 0xff);
+    }
 
-    // #[test]
-    // fn zero_page_x() {
-    //     let opcode = OPTABLE[0xB4];
-    //     assert_eq!(opcode.mode, ZeroPageX);
-    //     assert_eq!(opcode.name, OPCODE_NAME);
-    //
-    //     let (mut cpu, mut bus) = init();
-    //     cpu.sp = 0xff;
-    //     cpu.flags = 0b0000_0000;
-    //     cpu.y = 0b0000_1100;
-    //     cpu.pc = 0x0800;
-    //     cpu.x = 0x01;
-    //     bus.write_u8(cpu.pc + 1, 0x10);
-    //     bus.write_u8(0x11, 0b0000_1010);
-    //     let cycles = cpu.execute_instruction(opcode.opcode, &mut bus);
-    //     assert_eq!(cycles, opcode.cycles);
-    //     assert_eq!(cpu.y, 0b0000_1010);
-    //     assert_eq!(cpu.flags, 0b0000_0000);
-    //     assert_eq!(cpu.pc, 0x0802);
-    //     assert_eq!(cpu.sp, 0xff);
-    // }
-    //
-    // #[test]
-    // fn absolute() {
-    //     let opcode = OPTABLE[0xAC];
-    //     assert_eq!(opcode.mode, Absolute);
-    //     assert_eq!(opcode.name, OPCODE_NAME);
-    //
-    //     let (mut cpu, mut bus) = init();
-    //     cpu.sp = 0xff;
-    //     cpu.flags = 0b0000_0000;
-    //     cpu.y = 0b0000_1100;
-    //     cpu.pc = 0x0800;
-    //     bus.write_u16(cpu.pc + 1, 0x1234);
-    //     bus.write_u8(0x1234, 0b0000_1010);
-    //     let cycles = cpu.execute_instruction(opcode.opcode, &mut bus);
-    //     assert_eq!(cycles, opcode.cycles);
-    //     assert_eq!(cpu.y, 0b0000_1010);
-    //     assert_eq!(cpu.flags, 0b0000_0000);
-    //     assert_eq!(cpu.pc, 0x0803);
-    //     assert_eq!(cpu.sp, 0xff);
-    // }
-    //
-    // #[test]
-    // fn absolute_x() {
-    //     let opcode = OPTABLE[0xBC];
-    //     assert_eq!(opcode.mode, AbsoluteX);
-    //     assert_eq!(opcode.name, OPCODE_NAME);
-    //
-    //     // no page cross
-    //     let (mut cpu, mut bus) = init();
-    //     cpu.sp = 0xff;
-    //     cpu.flags = 0b0000_0000;
-    //     cpu.y = 0b0000_1100;
-    //     cpu.x = 0x1;
-    //     cpu.pc = 0x0800;
-    //     bus.write_u16(cpu.pc + 1, 0x1234);
-    //     bus.write_u8(0x1235, 0b0000_1010);
-    //     let cycles = cpu.execute_instruction(opcode.opcode, &mut bus);
-    //     assert_eq!(cycles, opcode.cycles);
-    //     assert_eq!(cpu.y, 0b0000_1010);
-    //     assert_eq!(cpu.flags, 0b0000_0000);
-    //     assert_eq!(cpu.pc, 0x0803);
-    //     assert_eq!(cpu.sp, 0xff);
-    //
-    //     // page cross (additional cycle)
-    //     let (mut cpu, mut bus) = init();
-    //     cpu.sp = 0xff;
-    //     cpu.flags = 0b0000_0000;
-    //     cpu.y = 0b0000_1100;
-    //     cpu.x = 0xff;
-    //     cpu.pc = 0x0800;
-    //     bus.write_u16(cpu.pc + 1, 0x1234);
-    //     bus.write_u8(0x1333, 0b0000_1010);
-    //     let cycles = cpu.execute_instruction(opcode.opcode, &mut bus);
-    //     assert_eq!(cycles, opcode.cycles + 1);
-    //     assert_eq!(cpu.y, 0b0000_1010);
-    //     assert_eq!(cpu.flags, 0b0000_0000);
-    //     assert_eq!(cpu.pc, 0x0803);
-    //     assert_eq!(cpu.sp, 0xff);
-    // }
+    #[test]
+    fn absolute() {
+        let opcode = OPTABLE[0xCC];
+        assert_eq!(opcode.mode, Absolute);
+        assert_eq!(opcode.name, OPCODE_NAME);
+
+        let (mut cpu, mut bus) = init();
+        cpu.sp = 0xff;
+        cpu.flags = 0b0000_0000;
+        cpu.y = 0x11;
+        cpu.pc = 0x0800;
+        bus.write_u16(cpu.pc + 1, 0x1234);
+        bus.write_u8(0x1234, 0x10);
+        let cycles = cpu.execute_instruction(opcode.opcode, &mut bus);
+        assert_eq!(cycles, opcode.cycles);
+        assert_eq!(cpu.y, 0x11);
+        assert_eq!(cpu.flags, 0b0000_0001);
+        assert_eq!(cpu.pc, 0x0803);
+        assert_eq!(cpu.sp, 0xff);
+    }
 }
